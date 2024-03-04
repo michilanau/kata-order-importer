d1da5ddcfae36c5a90deddba5ce06c0fa975b861
malanau <miglan99@gmail.com>
2024-03-04 19:16:03 +0100

    inital commit

---
+++ src/main/java/com/malanau/kataorderimporter/KataOrderImporterApplication.java
@@ -0,0 +1,13 @@
+package com.malanau.kataorderimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataOrderImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataOrderImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malanau/kataorderimporter/KataOrderImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malanau.kataorderimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataOrderImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
