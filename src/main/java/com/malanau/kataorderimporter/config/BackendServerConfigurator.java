bd9bab068bacba3f5f1da009c7734a62cc41de31
malanau <miglan99@gmail.com>
2024-03-12 00:03:43 +0100

    cors

--- src/main/java/com/malanau/kataorderimporter/config/BackendServerConfigurator.java
+++ src/main/java/com/malanau/kataorderimporter/config/BackendServerConfigurator.java
@@ -3,11 +3,23 @@ package com.malanau.kataorderimporter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BackendServerConfigurator {
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+      }
+    };
   }
 }
