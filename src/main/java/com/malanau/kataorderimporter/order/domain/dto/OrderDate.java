1e68e85af22898e4e68c9ced1985c4253666db33
malanau <miglan99@gmail.com>
2024-03-11 18:06:04 +0100

    Format date validation

--- src/main/java/com/malanau/kataorderimporter/order/domain/dto/OrderDate.java
+++ src/main/java/com/malanau/kataorderimporter/order/domain/dto/OrderDate.java
@@ -1,9 +1,22 @@
 package com.malanau.kataorderimporter.order.domain.dto;
 
 import com.malanau.kataorderimporter.shared.domain.StringValueObject;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class OrderDate extends StringValueObject {
   public OrderDate(String value) {
     super(value);
+    ensureIsValidOrderDate(value);
+  }
+
+  private void ensureIsValidOrderDate(String value) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    dateFormat.setLenient(false);
+    try {
+      dateFormat.parse(value);
+    } catch (ParseException e) {
+      throw new IllegalArgumentException(e);
+    }
   }
 }
--- src/main/java/com/malanau/kataorderimporter/order/domain/dto/OrderShipDate.java
+++ src/main/java/com/malanau/kataorderimporter/order/domain/dto/OrderShipDate.java
@@ -1,9 +1,22 @@
 package com.malanau.kataorderimporter.order.domain.dto;
 
 import com.malanau.kataorderimporter.shared.domain.StringValueObject;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class OrderShipDate extends StringValueObject {
   public OrderShipDate(String value) {
     super(value);
+    ensureIsValidOrderShipDate(value);
+  }
+
+  private void ensureIsValidOrderShipDate(String value) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    dateFormat.setLenient(false);
+    try {
+      dateFormat.parse(value);
+    } catch (ParseException e) {
+      throw new IllegalArgumentException(e);
+    }
   }
 }
